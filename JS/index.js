1aea8cd4096693c0f454e66932b4e711ac1debce
Cesar Guanipa <cesar10guanipa@gmail.com>
2023-02-25 17:48:42 -0300

    Refined recent article positioning (added more cards and js for dynamic font size) Added top article area with the respective cards

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,87 @@
-function fontAutosize () {
-    let title = document.getElementById('card-title')
-    let charCount = title.innerText.length
-    console.log(charCount)
+function fontAutosize1 () {
+    let title = document.getElementById("card-title1")
 
-    if (charCount <= 25) {
-        title.style.fontSize = "25px"
-    } else if (charCount > 25 && charCount <=35){
-        title.style.fontSize = "22px"
-    } else if (charCount > 35 && charCount <=50){
-        title.style.fontSize = "20px"
-    } else if (charCount > 50 && charCount <=65){
-        title.style.fontSize = "18px"
-    } else if (charCount > 65 && charCount <=75){
-        title.style.fontSize = "16px"
-    } else if (charCount > 75){
-        title.style.fontSize = "14px"
+        let charCount = title.innerText.length
+        console.log(charCount)
+    
+        if (charCount <= 25) {
+            title.style.fontSize = "25px"
+        } else if (charCount > 25 && charCount <=35){
+            title.style.fontSize = "22px"
+        } else if (charCount > 35 && charCount <=50){
+            title.style.fontSize = "20px"
+        } else if (charCount > 50 && charCount <=65){
+            title.style.fontSize = "18px"
+        } else if (charCount > 65 && charCount <=75){
+            title.style.fontSize = "16px"
+        } else if (charCount > 75){
+            title.style.fontSize = "14px"
+        }
     }
+function fontAutosize2 () {
+    let title = document.getElementById("card-title2")
+
+        let charCount = title.innerText.length
+        console.log(charCount)
+    
+        if (charCount <= 25) {
+            title.style.fontSize = "25px"
+        } else if (charCount > 25 && charCount <=35){
+            title.style.fontSize = "22px"
+        } else if (charCount > 35 && charCount <=50){
+            title.style.fontSize = "20px"
+        } else if (charCount > 50 && charCount <=65){
+            title.style.fontSize = "18px"
+        } else if (charCount > 65 && charCount <=75){
+            title.style.fontSize = "16px"
+        } else if (charCount > 75){
+            title.style.fontSize = "14px"
+        }
+    }
+
+function fontAutosize3 () {
+    let title = document.getElementById("card-title3")
+
+        let charCount = title.innerText.length
+        console.log(charCount)
+    
+        if (charCount <= 25) {
+            title.style.fontSize = "25px"
+        } else if (charCount > 25 && charCount <=35){
+            title.style.fontSize = "22px"
+        } else if (charCount > 35 && charCount <=50){
+            title.style.fontSize = "20px"
+        } else if (charCount > 50 && charCount <=65){
+            title.style.fontSize = "18px"
+        } else if (charCount > 65 && charCount <=75){
+            title.style.fontSize = "16px"
+        } else if (charCount > 75){
+            title.style.fontSize = "14px"
+        }
+    }
+
+function fontAutosize4 () {
+    let title = document.getElementById("card-title4")
+
+        let charCount = title.innerText.length
+        console.log(charCount)
+    
+        if (charCount <= 25) {
+            title.style.fontSize = "25px"
+        } else if (charCount > 25 && charCount <=35){
+            title.style.fontSize = "22px"
+        } else if (charCount > 35 && charCount <=50){
+            title.style.fontSize = "20px"
+        } else if (charCount > 50 && charCount <=65){
+            title.style.fontSize = "18px"
+        } else if (charCount > 65 && charCount <=75){
+            title.style.fontSize = "16px"
+        } else if (charCount > 75){
+            title.style.fontSize = "14px"
+        }
 }
 
-fontAutosize()
+fontAutosize1()
+fontAutosize2()
+fontAutosize3()
+fontAutosize4()
