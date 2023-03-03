7e9c5ce048f5dcba48bb864378dfb99069cb1972
Cesar Guanipa <cesar10guanipa@gmail.com>
2023-03-03 10:31:35 -0300

    finalized card style for dropdown menu in small view and added auto font size function for these cards

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
-function fontAutosize1 () {
-    let title = document.getElementById("card-title1")
+function fontAutosize () {
+    let titles = document.querySelectorAll(".card-title")
 
+    titles.forEach((title) => {
         let charCount = title.innerText.length
         console.log(charCount)
     
@@ -17,71 +18,36 @@ function fontAutosize1 () {
         } else if (charCount > 75){
             title.style.fontSize = "14px"
         }
-    }
-function fontAutosize2 () {
-    let title = document.getElementById("card-title2")
-
-        let charCount = title.innerText.length
-        console.log(charCount)
-    
-        if (charCount <= 25) {
-            title.style.fontSize = "25px"
-        } else if (charCount > 25 && charCount <=35){
-            title.style.fontSize = "22px"
-        } else if (charCount > 35 && charCount <=50){
-            title.style.fontSize = "20px"
-        } else if (charCount > 50 && charCount <=65){
-            title.style.fontSize = "18px"
-        } else if (charCount > 65 && charCount <=75){
-            title.style.fontSize = "16px"
-        } else if (charCount > 75){
-            title.style.fontSize = "14px"
-        }
+    })
     }
 
-function fontAutosize3 () {
-    let title = document.getElementById("card-title3")
-
-        let charCount = title.innerText.length
-        console.log(charCount)
+    function fontAutosizeSmallView () {
+        let titles = document.querySelectorAll(".list-card-title")
     
-        if (charCount <= 25) {
-            title.style.fontSize = "25px"
-        } else if (charCount > 25 && charCount <=35){
-            title.style.fontSize = "22px"
-        } else if (charCount > 35 && charCount <=50){
-            title.style.fontSize = "20px"
-        } else if (charCount > 50 && charCount <=65){
-            title.style.fontSize = "18px"
-        } else if (charCount > 65 && charCount <=75){
-            title.style.fontSize = "16px"
-        } else if (charCount > 75){
-            title.style.fontSize = "14px"
+        titles.forEach((title) => {
+            let charCount = title.innerText.length
+            console.log(charCount)
+        
+            if (charCount <= 25) {
+                title.style.fontSize = "6.5vw"
+            } else if (charCount > 25 && charCount <=35){
+                title.style.fontSize = "6vw"
+            } else if (charCount > 35 && charCount <=50){
+                title.style.fontSize = "5.5vw"
+            } else if (charCount > 50 && charCount <=65){
+                title.style.fontSize = "5vw"
+            } else if (charCount > 65 && charCount <=75){
+                title.style.fontSize = "4.5vw"
+            } else if (charCount > 75){
+                title.style.fontSize = "4vw"
+            }   
+        })
         }
+
+    function activateFlex(){
+        let toggle = 0
+        let container = document.querySelectorAll(".top-article-dropdown-menu")
+
     }
-
-function fontAutosize4 () {
-    let title = document.getElementById("card-title4")
-
-        let charCount = title.innerText.length
-        console.log(charCount)
-    
-        if (charCount <= 25) {
-            title.style.fontSize = "25px"
-        } else if (charCount > 25 && charCount <=35){
-            title.style.fontSize = "22px"
-        } else if (charCount > 35 && charCount <=50){
-            title.style.fontSize = "20px"
-        } else if (charCount > 50 && charCount <=65){
-            title.style.fontSize = "18px"
-        } else if (charCount > 65 && charCount <=75){
-            title.style.fontSize = "16px"
-        } else if (charCount > 75){
-            title.style.fontSize = "14px"
-        }
-}
-
-fontAutosize1()
-fontAutosize2()
-fontAutosize3()
-fontAutosize4()
+fontAutosize()
+fontAutosizeSmallView()
