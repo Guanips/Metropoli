e12e218b0a321a01bc269412a3a9d2c7d74dc95e
Cesar Guanipa <cesar10guanipa@gmail.com>
2023-03-05 22:24:41 -0300

    added new nav bar style for small screens added view all buttons to small screen view

--- JS/index.js
+++ JS/index.js
@@ -19,35 +19,30 @@ function fontAutosize () {
             title.style.fontSize = "14px"
         }
     })
-    }
+}
 
-    function fontAutosizeSmallView () {
-        let titles = document.querySelectorAll(".list-card-title")
+function fontAutosizeSmallView () {
+    let titles = document.querySelectorAll(".list-card-title")
+
+    titles.forEach((title) => {
+        let charCount = title.innerText.length
+        console.log(charCount)
     
-        titles.forEach((title) => {
-            let charCount = title.innerText.length
-            console.log(charCount)
-        
-            if (charCount <= 25) {
-                title.style.fontSize = "6.5vw"
-            } else if (charCount > 25 && charCount <=35){
-                title.style.fontSize = "6vw"
-            } else if (charCount > 35 && charCount <=50){
-                title.style.fontSize = "5.5vw"
-            } else if (charCount > 50 && charCount <=65){
-                title.style.fontSize = "5vw"
-            } else if (charCount > 65 && charCount <=75){
-                title.style.fontSize = "4.5vw"
-            } else if (charCount > 75){
-                title.style.fontSize = "4vw"
-            }   
-        })
-        }
+        if (charCount <= 25) {
+            title.style.fontSize = "6.5vw"
+        } else if (charCount > 25 && charCount <=35){
+            title.style.fontSize = "6vw"
+        } else if (charCount > 35 && charCount <=50){
+            title.style.fontSize = "5.5vw"
+        } else if (charCount > 50 && charCount <=65){
+            title.style.fontSize = "5vw"
+        } else if (charCount > 65 && charCount <=75){
+            title.style.fontSize = "4.5vw"
+        } else if (charCount > 75){
+            title.style.fontSize = "4vw"
+        }   
+    })
+}
 
-    function activateFlex(){
-        let toggle = 0
-        let container = document.querySelectorAll(".top-article-dropdown-menu")
-
-    }
 fontAutosize()
 fontAutosizeSmallView()
